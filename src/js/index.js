7b2e05e11f49c495c5d322dbc48e4b0f6cd49f20
Alexey <laiten808@gmail.com>
2018-01-19 04:46:33 +0400

    fix footer, add polyphile for ie sticky

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 ﻿// --- Библиотеки
 //= helpers/mobile_detect/mobile-detect.min.js
+//= helpers/polyphile/stickyfill.min.js
 
 var isScrolling; //получает значение из objMobileDetect для catchEventScroll
 
@@ -10,6 +11,23 @@ var isScrolling; //получает значение из objMobileDetect для
 //= helpers/floatColorMenu.js
 //= helpers/reviewsNextPages.js
 //= helpers/slideArrrowBtn.js
+
+
+
+//проверка на IE
+var ua = window.navigator.userAgent.toLowerCase(),
+is_ie = (/trident/gi).test(ua) || (/msie/gi).test(ua);
+
+if (is_ie) {
+    //откл анимацию
+    isScrolling = true;
+
+    //полифил для ie 
+    var elements = document.querySelectorAll('.header');
+    Stickyfill.add(elements);
+}
+
+
 
 window.onscroll = function() {
 	FloatColorMenu (header); //анимация меню при скролле
